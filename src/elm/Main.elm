2153e0b11414e4cacd5bf0ff5213b1bf4c7f65e8
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2019-10-22 02:41:21 +0200

    upgraded elm and ran elm-format

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,64 +1,90 @@
 module Main exposing (..)
-import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
 
 -- component import example
-import Components.Hello exposing ( hello )
+
+import Browser
+import Components.Hello exposing (hello)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
 
 
 -- APP
+
+
 main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, view = view, update = update }
+    Browser.sandbox { init = init, view = view, update = update }
+
 
 
 -- MODEL
-type alias Model = Int
+
+
+type alias Model =
+    Int
+
 
 init : Model
-init = 0
+init =
+    0
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+
+
+type Msg
+    = NoOp
+    | Increment
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        NoOp ->
+            model
+
+        Increment ->
+            model + 1
+
 
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style "margin-top" "30px", style "text-align" "center" ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img (src "static/img/elm.jpg" :: styles.img) []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div [ class "container", style "margin-top" "30px", style "text-align" "center" ]
+        [ -- inline CSS (literal)
+          div [ class "row" ]
+            [ div [ class "col-xs-12" ]
+                [ div [ class "jumbotron" ]
+                    [ img (src "static/img/elm.jpg" :: styles.img) [] -- inline CSS (via var)
+                    , hello model -- ext 'hello' component (takes 'model' as arg)
+                    , p [] [ text "Elm Webpack Starter" ]
+                    , button [ class "btn btn-primary btn-lg", onClick Increment ]
+                        [ -- click handler
+                          span [ class "glyphicon glyphicon-star" ] [] -- glyphicon
+                        , span [] [ text "FTW!" ]
+                        ]
+                    ]
+                ]
+            ]
         ]
-      ]
-    ]
-  ]
+
 
 
 -- CSS STYLES
-styles : { img : List ( Attribute msg) }
+
+
+styles : { img : List (Attribute msg) }
 styles =
-  {
-    img =
-      [ style "width" "33%"
-      , style "border" "4px solid #337AB7"
-      ]
-  }
+    { img =
+        [ style "width" "33%"
+        , style "border" "4px solid #337AB7"
+        ]
+    }
